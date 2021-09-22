895ffa96c7b43d3e17675542662bee200f38e519
eartser <eartser19@gmail.com>
2021-09-22 15:18:07 +0300

    handle updating ViewState

--- app/src/main/java/com/elizavetaartser/androidproject/MainActivity.kt
+++ app/src/main/java/com/elizavetaartser/androidproject/MainActivity.kt
@@ -2,36 +2,55 @@ package com.elizavetaartser.androidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.view.View
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.squareup.moshi.Moshi
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import okhttp3.Dispatcher
-import okhttp3.OkHttpClient
-import kotlin.collections.listOf as listOf
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.elizavetaartser.androidproject.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collect
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    companion object {
+        val LOG_TAG = "MainActivityTag"
+    }
+
+    private val viewModel: MainViewModel by viewModels()
+
+    private val viewBinding by viewBinding(ActivityMainBinding::bind)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val adapter = setupRecyclerView()
-        findViewById<View>(R.id.usersRecyclerView).isVisible = false
-        findViewById<View>(R.id.progressBar).isVisible = true
+        Log.d(LOG_TAG, "onCreate()")
+        setupRecyclerView()
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                adapter.userList = loadUsers()
-                adapter.notifyDataSetChanged()
-                findViewById<View>(R.id.usersRecyclerView).isVisible = true
-                findViewById<View>(R.id.progressBar).isVisible = false
+                viewModel.viewState.collect { viewState ->
+                    Log.d(LOG_TAG, "$viewState")
+                    renderViewState(viewState)
+                }
+            }
+        }
+    }
+
+    private fun renderViewState(viewState: MainViewModel.ViewState) {
+        when (viewState) {
+            is MainViewModel.ViewState.Loading -> {
+                viewBinding.usersRecyclerView.isVisible = false
+                viewBinding.progressBar.isVisible = true
+            }
+            is MainViewModel.ViewState.Data -> {
+                viewBinding.usersRecyclerView.isVisible = true
+                (viewBinding.usersRecyclerView.adapter as UserAdapter).apply {
+                    userList = viewState.userlist
+                    notifyDataSetChanged()
+                }
+                viewBinding.progressBar.isVisible = false
             }
         }
     }
@@ -42,30 +61,4 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         recyclerView.adapter = adapter
         return adapter
     }
-
-    private suspend fun loadUsers(): List<User> {
-        return withContext(Dispatchers.IO) {
-            Thread.sleep(3000)
-            provideApi().getUsers().data
-        }
-    }
-
-    private fun provideApi(): Api {
-        return Retrofit.Builder()
-            .client(provideOkHttpClient())
-            .baseUrl("https://reqres.in/api/")
-            .addConverterFactory(MoshiConverterFactory.create(provideMoshi()))
-            .build()
-            .create(Api::class.java)
-    }
-
-    private fun provideOkHttpClient(): OkHttpClient {
-        return OkHttpClient.Builder().build()
-    }
-
-    private fun provideMoshi(): Moshi {
-        return Moshi.Builder().build()
-    }
-
-
 }
