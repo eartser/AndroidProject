e4613b3ea0998a0be0bae760cbe81a8fb2df1a77
eartser <eartser19@gmail.com>
2021-10-13 15:16:25 +0300

    make userlist fragment

--- app/src/main/java/com/elizavetaartser/androidproject/MainActivity.kt
+++ app/src/main/java/com/elizavetaartser/androidproject/MainActivity.kt
@@ -1,65 +1,14 @@
 package com.elizavetaartser.androidproject
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
-import androidx.core.view.isVisible
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.launch
+import androidx.appcompat.app.AppCompatActivity
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.elizavetaartser.androidproject.databinding.ActivityMainBinding
-import kotlinx.coroutines.flow.collect
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
-    companion object {
-        val LOG_TAG = "MainActivityTag"
-    }
 
     private val viewModel: MainViewModel by viewModels()
 
     private val viewBinding by viewBinding(ActivityMainBinding::bind)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        supportFragmentManager
-        Log.d(LOG_TAG, "onCreate()")
-        setupRecyclerView()
-        lifecycleScope.launch {
-            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.viewState.collect { viewState ->
-                    Log.d(LOG_TAG, "$viewState")
-                    renderViewState(viewState)
-                }
-            }
-        }
-    }
-
-    private fun renderViewState(viewState: MainViewModel.ViewState) {
-        when (viewState) {
-            is MainViewModel.ViewState.Loading -> {
-                viewBinding.usersRecyclerView.isVisible = false
-                viewBinding.progressBar.isVisible = true
-            }
-            is MainViewModel.ViewState.Data -> {
-                viewBinding.usersRecyclerView.isVisible = true
-                (viewBinding.usersRecyclerView.adapter as UserAdapter).apply {
-                    userList = viewState.userlist
-                    notifyDataSetChanged()
-                }
-                viewBinding.progressBar.isVisible = false
-            }
-        }
-    }
-
-    private fun setupRecyclerView(): UserAdapter {
-        val recyclerView = findViewById<RecyclerView>(R.id.usersRecyclerView)
-        val adapter = UserAdapter()
-        recyclerView.adapter = adapter
-        return adapter
-    }
 }
