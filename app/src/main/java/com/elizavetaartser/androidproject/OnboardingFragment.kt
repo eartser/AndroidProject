6b9dc38dcc5a563d7e4c7ca1ae8b5d3261cd5891
eartser <eartser19@gmail.com>
2021-10-13 16:44:49 +0300

    add onboarding screen

--- app/src/main/java/com/elizavetaartser/androidproject/OnboardingFragment.kt
+++ app/src/main/java/com/elizavetaartser/androidproject/OnboardingFragment.kt
@@ -2,13 +2,18 @@ package com.elizavetaartser.androidproject
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.elizavetaartser.androidproject.databinding.FragmentOnboardingBinding
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
 
 class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
 
@@ -16,14 +21,28 @@ class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
 
     private var player: ExoPlayer? = null
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         player = SimpleExoPlayer.Builder(requireContext()).build().apply {
             addMediaItem(MediaItem.fromUri("asset:///onboarding.mp4"))
             repeatMode = Player.REPEAT_MODE_ALL
             prepare()
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewBinding.playerView.player = player
+        viewBinding.viewPager.setTextPages()
+        viewBinding.viewPager.attachDots(viewBinding.onboardingTextTabLayout)
+        viewBinding.signInButton.setOnClickListener {
+            // TODO: Go to SignInFragment.
+            Toast.makeText(requireContext(), "Нажата кнопка войти", Toast.LENGTH_SHORT).show()
+        }
+        viewBinding.signUpButton.setOnClickListener {
+            // TODO: Go to SignUpFragment.
+            Toast.makeText(requireContext(), "Нажата кнопка зарегистрироваться", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onResume() {
@@ -39,6 +58,22 @@ class OnboardingFragment : Fragment(R.layout.fragment_onboarding) {
     override fun onDestroy() {
         super.onDestroy()
         player?.release()
+    }
+
+    private fun ViewPager2.setTextPages() {
+        adapter =
+            ListDelegationAdapter(onboardingTextAdapterDelegate()).apply {
+                items =
+                    listOf(
+                        getString(R.string.onboarding_view_pager_text_1),
+                        getString(R.string.onboarding_view_pager_text_2),
+                        getString(R.string.onboarding_view_pager_text_3)
+                    )
+            }
+    }
+
+    private fun ViewPager2.attachDots(tabLayout: TabLayout) {
+        TabLayoutMediator(tabLayout, this) { _, _ -> }.attach()
     }
 
 }
