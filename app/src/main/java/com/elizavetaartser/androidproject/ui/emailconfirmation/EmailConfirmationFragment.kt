eb19025cbf2feedb02e680644e18474795a04605
eartser <eartser19@gmail.com>
2021-11-10 15:42:28 +0300

    add email confirmation code input

--- app/src/main/java/com/elizavetaartser/androidproject/ui/emailconfirmation/EmailConfirmationFragment.kt
+++ app/src/main/java/com/elizavetaartser/androidproject/ui/emailconfirmation/EmailConfirmationFragment.kt
@@ -1,6 +1,9 @@
 package com.elizavetaartser.androidproject.ui.emailconfirmation
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.elizavetaartser.androidproject.R
 import com.elizavetaartser.androidproject.databinding.FragmentEmailConfirmationBinding
@@ -11,4 +14,11 @@ class EmailConfirmationFragment : BaseFragment(R.layout.fragment_email_confirmat
     private val viewBinding by viewBinding(FragmentEmailConfirmationBinding::bind)
 
     private val viewModel: EmailConfirmationViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
