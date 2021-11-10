a4a76ae300ddb96d6f0c98add0d159514cf255ac
eartser <eartser19@gmail.com>
2021-11-10 13:49:12 +0300

    add sign in and sign up screens

--- app/src/main/java/com/elizavetaartser/androidproject/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/elizavetaartser/androidproject/ui/signup/SignUpViewModel.kt
@@ -1,7 +1,47 @@
 package com.elizavetaartser.androidproject.ui.signup
 
+import androidx.lifecycle.viewModelScope
+import com.elizavetaartser.androidproject.repository.AuthRepository
 import com.elizavetaartser.androidproject.ui.base.BaseViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 class SignUpViewModel : BaseViewModel() {
 
+    private val _eventChannel = Channel<Event>(Channel.BUFFERED)
+
+    fun eventsFlow(): Flow<Event> {
+        return _eventChannel.receiveAsFlow()
+    }
+
+    fun signUp(
+        firstname: String,
+        lastname: String,
+        login: String,
+        email: String,
+        password: String
+    ) {
+        viewModelScope.launch {
+            try {
+                AuthRepository.signUp(
+                    firstname,
+                    lastname,
+                    login,
+                    email,
+                    password
+                )
+                // _eventChannel.send(Event.SignUpSuccess)
+                _eventChannel.send(Event.SignUpEmailConfirmationRequired)
+            } catch (error: Exception) {
+                _eventChannel.send(Event.SignUpEmailConfirmationRequired)
+            }
+        }
+    }
+
+    sealed class Event {
+        object SignUpSuccess : Event()
+        object SignUpEmailConfirmationRequired : Event()
+    }
 }
