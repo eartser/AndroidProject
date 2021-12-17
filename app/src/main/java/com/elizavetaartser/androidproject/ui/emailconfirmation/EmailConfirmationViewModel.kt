da7d4ebf7926542f9a04c1e507dfe5ea342eff45
eartser <eartser19@gmail.com>
2021-12-17 13:47:20 +0300

    implement email confirmation

--- app/src/main/java/com/elizavetaartser/androidproject/ui/emailconfirmation/EmailConfirmationViewModel.kt
+++ app/src/main/java/com/elizavetaartser/androidproject/ui/emailconfirmation/EmailConfirmationViewModel.kt
@@ -1,7 +1,21 @@
 package com.elizavetaartser.androidproject.ui.emailconfirmation
 
+import androidx.lifecycle.viewModelScope
+import com.elizavetaartser.androidproject.interactor.AuthInteractor
 import com.elizavetaartser.androidproject.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class EmailConfirmationViewModel : BaseViewModel() {
+@HiltViewModel
+class EmailConfirmationViewModel @Inject constructor(
+    private val authInteractor: AuthInteractor
+): BaseViewModel() {
+
+    fun signIn(email: String, password: String) {
+        viewModelScope.launch {
+            authInteractor.signInWithEmail(email, password)
+        }
+    }
 
 }
